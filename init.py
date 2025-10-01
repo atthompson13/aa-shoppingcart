d36a3e3eeb360acdd95b1c192136704c92dd7c58
Austin <austin.thompson1@proton.me>
2025-10-01 17:29:19 -0600

    Initialize init.py with project metadata

---
+++ init.py
@@ -0,0 +1,9 @@
+"""
+Alliance Auth Shopping Cart
+A player-to-player shopping cart and contract marketplace for Alliance Auth
+"""
+
+__version__ = "0.1.0"
+__title__ = "Alliance Auth Shopping Cart"
+
+default_app_config = "shopping_cart.apps.ShoppingCartConfig"
