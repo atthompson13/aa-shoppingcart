8d61a35c7da5ec3941c11ad60c1d15e218d2a1e8
Austin <austin.thompson1@proton.me>
2025-10-01 22:35:31 -0600

    Refactor shopping cart app configuration

--- shopping_cart/__init__.py
+++ shopping_cart/__init__.py
@@ -1,4 +1,7 @@
-"""Alliance Auth Shopping Cart"""
-__version__ = "0.1.0"
-__title__ = "Alliance Auth Shopping Cart"
-default_app_config = "shopping_cart.apps.ShoppingCartConfig"
+# shopping_cart/apps.py
+from django.apps import AppConfig
+
+class ShoppingCartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shopping_cart'
+    verbose_name = 'Shopping Cart'
